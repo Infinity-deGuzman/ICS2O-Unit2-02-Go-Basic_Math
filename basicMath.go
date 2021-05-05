7ab9760cffaa4d6f532cf4830d058ccb5c1e29fd
InfinitydeGuzma <>
2021-05-05 20:30:45 +0000

    .

---
+++ basicMath.go
@@ -0,0 +1,17 @@
+// Created by: Infinity de Guzman
+// Created on: May 2021
+//
+// This program does basic math
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This function does basic math
+  fmt.Println("The dimensions of a rectangle are: 5cm and 3cm.")
+	fmt.Println("The area is:", (5*3), "cm²")
+	fmt.Println("The perimeter is:", (2*(5+3)), "cm")
+}
